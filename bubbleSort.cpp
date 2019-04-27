6e86dfe95b5259af0115f21cd3c745f3028daec3
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2019-04-27 12:05:45 -0500

    C++ Bubble Sort module added

---
+++ bubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//Function to swap two elements in an array
+void swap(NumericVector array, int a, int b)
+{
+  float a_foo = array[a];
+  array[a] = array[b];
+  array[b] = a_foo;
+}
+
+//Function to perform a bubble sort on an
+// [[Rcpp::export]]
+NumericVector bubbleSort(NumericVector x) {
+    //Get size of vector x
+    int n = x.size();
+    //For every element in vector x
+    for (int i = 0; i < n; i++)
+    {
+        //For every element in vector x (excluding the last i-1 elements)
+        for (int j = 0; j < n - i -1; j++)
+        {
+          float a = x[j];
+          float b = x[j+1];
+          
+          //if a > b, perform a swap in the array
+          if (a > b) 
+          {
+            swap(x, j, j+1);
+          }
+        }
+    }
+    return x;
+}
+
